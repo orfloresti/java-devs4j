7594391050747ab0958ec7b5c4de784f3d711c21
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2021-10-05 12:01:14 -0500

    Examples of profiles

--- 2-dependency-injection/src/main/java/com/example/di/Application.java
+++ 2-dependency-injection/src/main/java/com/example/di/Application.java
@@ -1,18 +1,22 @@
 package com.example.di;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+// import com.example.di.profiles.EnviromentService;
 // import com.example.di.qualifiers.Animal;
-//import com.example.di.attribute.Coche;
+// import com.example.di.attribute.Coche;
 // import com.example.di.qualifiers.Animal;
-import com.example.di.qualifiers.Nido;
-//import com.example.di.qualifiers.Avion;
-//import com.example.di.qualifiers.Pajaro;
-//import com.example.di.qualifiers.Perro;
+// import com.example.di.qualifiers.Nido;
+// import com.example.di.qualifiers.Avion;
+// import com.example.di.qualifiers.Pajaro;
+// import com.example.di.qualifiers.Perro;
+// import com.example.di.scopes.EjemploScopeService;
 
 // import com.example.di.attribute.Coche;
 // import com.example.di.attribute.Motor;
@@ -20,17 +24,42 @@ import com.example.di.qualifiers.Nido;
 @SpringBootApplication
 public class Application {
 
-	
-	// private static final Logger log = LoggerFactory.getLogger(Application.class);
-	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	@Bean
+	public String getApplicationName() {
+		return "¡Devs4j rules!";
+	}
+
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class);
 		
-		Nido nido = context.getBean(Nido.class);
-		// log.info("Animal | nombre = {}, edad = {}", nido.getNombre(), nido.getEdad());
-		nido.imprimir();
+		/*
+		 * String nombreAplicacion = context.getBean(String.class);
+		 * log.info("Nombre aplicacion {}", nombreAplicacion);
+		 */
 		
-		// context.getBean("qualifier", Animal.class);
+		// EjemploScopeService ejemploScopeService =
+		// context.getBean(EjemploScopeService.class);
+		// EjemploScopeService ejemploScopeService1 =
+		// context.getBean(EjemploScopeService.class);
+
+		// log.info("Are beans equals {} ",
+		// ejemploScopeService.equals(ejemploScopeService1));
+		// log.info("Are beans == {} ", ejemploScopeService == ejemploScopeService1 );
+
+		// EnviromentService environmentService =
+		// context.getBean(EnviromentService.class);
+
+		// log.info("Active enviroment", environmentService.getEnviroment() );
+		// Nido nido = context.getBean(Nido.class);
+
+		// // log.info("Animal | nombre = {}, edad = {}", nido.getNombre(),
+		// nido.getEdad());
+
+		// nido.imprimir();
+
+		// // context.getBean("qualifier", Animal.class);
 	}
 
 }
--- 2-dependency-injection/src/main/java/com/example/di/profiles/DevEnviroment.java
+++ 2-dependency-injection/src/main/java/com/example/di/profiles/DevEnviroment.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("dev")
+@Profile( value = {"dev", "default"})
 public class DevEnviroment implements EnviromentService {
 
     @Override
